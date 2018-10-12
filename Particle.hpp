e65edb48397bac6e74a4e14ff69f089fea01c9ce
Ian Squiers <iansquiers321@gmail.com>
2018-10-12 12:51:03 -0400

    Files added, need to think about proper structure

---
+++ Particle.hpp
@@ -0,0 +1,14 @@
+//
+//  Particle.hpp
+//  Proj2
+//
+//  Created by Ian Squiers on 10/12/18.
+//  Copyright © 2018 Ian Squiers. All rights reserved.
+//
+
+#ifndef Particle_hpp
+#define Particle_hpp
+
+#include <stdio.h>
+
+#endif /* Particle_hpp */
