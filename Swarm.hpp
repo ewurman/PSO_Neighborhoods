5e98e094e4d771e20df29edeb0863a0bde5eb6c2
Ian Squiers <iansquiers321@gmail.com>
2018-10-13 13:45:02 -0400

    topology implimentation

--- Swarm.hpp
+++ Swarm.hpp
@@ -5,9 +5,45 @@
 //  Created by Ian Squiers and Erik Wurman on 10/12/18.
 //
 
+//
+//  Particle.hpp
+//  Proj2
+//
+//  Created by Ian Squiers and Erik Wurman on 10/12/18.
+//
+
 #ifndef Swarm_hpp
 #define Swarm_hpp
 
 #include <stdio.h>
+#include <vector>
+#include "utils.cpp"
 
-#endif /* Swarm_hpp */
+
+class Swarm {
+    static double constrictionFactor = 0.7298;
+    static double towardPbest = 2.05;
+    static double towardNbest = 2.05;
+
+    private:
+        
+        
+        
+    public:
+        Particle(int dimensions);
+        Particle(int dimensions, vector<Particle> neighborhood);
+
+        double getPBestVal() { return this.pbestVal; }
+        double* getPBestLoc() { return this.pbestLoc; }
+        double* getPosition() { return this.position; }
+        double* getVelocity() { return this.velocity; }
+
+
+        double* getNBestLoc();
+        void update_position();
+        void update_velocity(); //maybe parameters like inertia rate, etc or make them constants
+};
+
+
+
+#endif /* Particle_hpp */
--- utils.cpp
+++ utils.cpp
@@ -5,11 +5,100 @@
 
 #ifndef UTILS_CPP
 #define UTILS_CPP
+#include "Particle.hpp"
+#include <vector>
 
 double randomDoubleInRange(double fMin, double fMax)
 {
     double f = (double)rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
+}
+
+//ARE you included in your own neighborhood?
+
+void ring(vector<Particle> particles) {
+	for (int i = 0; i < particles.size(); i++) {
+		if (i == 0){
+			particles[i].neighborhood.pushback(&particles[particles.size()-1]);
+			particles[i].neighborhood.pushback(&particles[i+1]);
+		}
+		else if (i == (particles.size() - 1)){
+			particles[i].neighborhood.pushback(&particles[i-1]);
+			particles[i].neighborhood.pushback(&particles[i+1]);
+		}
+		else {
+			particles[i].neighborhood.pushback(&particles[i+1]);
+			particles[i].neighborhood.pushback(&particles[i-1]);
+		}
+	}
+}
+
+void vonNeumann(vector<Particle> particles) {
+	for (int i = 0; i < particles.size(); i++) {
+		if (i == 0){
+			particles[i].neighborhood.pushback(&particles[particles.size()-2]);
+			particles[i].neighborhood.pushback(&particles[particles.size()-1]);
+			particles[i].neighborhood.pushback(&particles[i+1]);
+			particles[i].neighborhood.pushback(&particles[i+2]);
+		}
+		if (i == 1){
+			particles[i].neighborhood.pushback(&particles[particles.size()-1]);
+			particles[i].neighborhood.pushback(&particles[i-1]);
+			particles[i].neighborhood.pushback(&particles[i+1]);
+			particles[i].neighborhood.pushback(&particles[i+2]);
+		}
+		else if (i == (particles.size() - 2)){
+			particles[i].neighborhood.pushback(&particles[i-2]);
+			particles[i].neighborhood.pushback(&particles[i-1]);
+			particles[i].neighborhood.pushback(&particles[i+1]);
+			particles[i].neighborhood.pushback(&particles[0]);			
+		}		
+
+		else if (i == (particles.size() - 1)){
+			particles[i].neighborhood.pushback(&particles[i-2]);
+			particles[i].neighborhood.pushback(&particles[i-1]);
+			particles[i].neighborhood.pushback(&particles[0]);
+			particles[i].neighborhood.pushback(&particles[1]);			
+		}	
+
+		else {
+			particles[i].neighborhood.pushback(&particles[i-2]);
+			particles[i].neighborhood.pushback(&particles[i-1]);
+			particles[i].neighborhood.pushback(&particles[i+1]);
+			particles[i].neighborhood.pushback(&particles[i+2]);
+		}
+	}
+}
+
+void global(vector<Particle> particles) {
+	for (int i = 0; i < particles.size(); i++) {
+		for (int j = 0; j < particles.size(); j++) {
+			if (i != j) {
+				particles[i].neighborhood.pushback(&particles[j])
+			}
+		}
+	}
+}
+
+void randNeighbors(vector<Particle> particles, int numNeighbors) {
+	vector<int> used;
+	std::vector<int>::iterator it;
+	for (int i = 0; i < particles.size(); i++) {
+		for (int j = 0; j < numNeighbors; j++) {
+			int r = rand() % particles.size();
+
+			it = find(used.begin(), used.end(), r);
+
+
+			while (it != used.end()) {
+				int r = rand() % particles.size();
+				it = find(used.begin(), used.end(), r);
+			}
+
+			particles[i].neighborhood.pushback(&particles[r]);
+			used.pushback(r);
+		}
+	}
 }
 
 
