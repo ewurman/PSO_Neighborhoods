57594c4942c063bd3d2a4628a02a0b87bb947773
Ian Squiers <iansquiers321@gmail.com>
2018-10-13 15:08:47 -0400

    working out merge

--- main.cpp
+++ main.cpp
@@ -10,15 +10,49 @@
 #include "Particle.cpp"
 #include "Swarm.cpp"
 
+using namespace std;
+
 enum Function {
     rosenbrock,
     ackley,
     rastrigin
 }
 
+enum Topology {
+	global,
+	ring,
+	von_Neumann,
+	random
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+	char * topology = argv[1];
+	char * num_particles = atof(argv[2]);
+	int iterations = atof(argv[3]);
+	char * func = argv[4];
+	int * dimensions = atof(argv[5]);
+
+
+	// initialize particles
+
+	// establish neighborhoods
+    if (!strcmp(argv[1], "gl")) {
+    } else if (!strcmp(topology, "ri")) {
+    } else if (!strcmp(topology, "vn")) {
+    } else if (!strcmp(topology, "ra")) {
+    } else {
+        cout << "Enter valid Topology method ( gl, ri, vn, ra )" << endl;
+        return null;
+    }
+
+    // run interations
+    for (int i = 0; i < interations; i++) {
+    	
+    }
+
+    // find best so far
+
+
     return 0;
 }
--- utils.cpp
+++ utils.cpp
@@ -14,7 +14,60 @@ double randomDoubleInRange(double fMin, double fMax)
     return fMin + f * (fMax - fMin);
 }
 
-//ARE you included in your own neighborhood?
+void vonNeumann(vector<Particle> particles) {
+	int rows;
+	switch (particles.size()) {
+		case 16 : rows = 4; break;
+		case 30 : rows = 5; break;
+		case 49 : rows = 7; break;
+	}
+	int columns = particles.size()/rows;
+	int ** vn = vnArray(rows, columns);
+	int rup, rdown, cr, cl;
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < rows; j++) {
+			rup = i + 1;
+			rdown = i - 1;
+			cl = j - 1;
+			cr = j + 1;
+
+			if (i = 0) {
+				rup = rows - 1;
+				rdown = i + 1;
+			}
+			if (i = (rows-1)) {
+				rup = i - 1;
+				rdown = 0;
+			}
+			if (j = 0){
+				cl = columns - 1;
+				cr = j + 1;
+			}
+			if (j == columns - 1) {
+				cl = j - 1;
+				cr = 0
+			}
+
+			particles[vn[i][j]].neighborhood.pushback(&particles[rup][j]); // up
+			particles[vn[i][j]].neighborhood.pushback(&particles[rdown][j]); // down
+			particles[vn[i][j]].neighborhood.pushback(&particles[i][cr]); // right
+			particles[vn[i][j]].neighborhood.pushback(&particles[i][cl]); // left
+		}
+	}
+}
+
+int** vnArray(int rows, int columns) {
+	int ** indecies = new int*[rows];
+	int count = 0;
+	for (int i = 0; i < rows; i++) {
+		indecies[i] = new int[columns];
+		for (int j = 0; j < columns; j++) {
+			indecies[i][j] = count;
+			count++;
+		}
+	}
+}
 
 void ring(vector<Particle> particles) {
 	for (int i = 0; i < particles.size(); i++) {
@@ -33,42 +86,6 @@ void ring(vector<Particle> particles) {
 	}
 }
 
-void vonNeumann(vector<Particle> particles) {
-	for (int i = 0; i < particles.size(); i++) {
-		if (i == 0){
-			particles[i].neighborhood.pushback(&particles[particles.size()-2]);
-			particles[i].neighborhood.pushback(&particles[particles.size()-1]);
-			particles[i].neighborhood.pushback(&particles[i+1]);
-			particles[i].neighborhood.pushback(&particles[i+2]);
-		}
-		if (i == 1){
-			particles[i].neighborhood.pushback(&particles[particles.size()-1]);
-			particles[i].neighborhood.pushback(&particles[i-1]);
-			particles[i].neighborhood.pushback(&particles[i+1]);
-			particles[i].neighborhood.pushback(&particles[i+2]);
-		}
-		else if (i == (particles.size() - 2)){
-			particles[i].neighborhood.pushback(&particles[i-2]);
-			particles[i].neighborhood.pushback(&particles[i-1]);
-			particles[i].neighborhood.pushback(&particles[i+1]);
-			particles[i].neighborhood.pushback(&particles[0]);			
-		}		
-
-		else if (i == (particles.size() - 1)){
-			particles[i].neighborhood.pushback(&particles[i-2]);
-			particles[i].neighborhood.pushback(&particles[i-1]);
-			particles[i].neighborhood.pushback(&particles[0]);
-			particles[i].neighborhood.pushback(&particles[1]);			
-		}	
-
-		else {
-			particles[i].neighborhood.pushback(&particles[i-2]);
-			particles[i].neighborhood.pushback(&particles[i-1]);
-			particles[i].neighborhood.pushback(&particles[i+1]);
-			particles[i].neighborhood.pushback(&particles[i+2]);
-		}
-	}
-}
 
 void global(vector<Particle> particles) {
 	for (int i = 0; i < particles.size(); i++) {
@@ -100,6 +117,44 @@ void randNeighbors(vector<Particle> particles, int numNeighbors) {
 		}
 	}
 }
+
+
+// void vonNeumann(vector<Particle> particles) {
+// 	for (int i = 0; i < particles.size(); i++) {
+// 		if (i == 0){
+// 			particles[i].neighborhood.pushback(&particles[particles.size()-2]);
+// 			particles[i].neighborhood.pushback(&particles[particles.size()-1]);
+// 			particles[i].neighborhood.pushback(&particles[i+1]);
+// 			particles[i].neighborhood.pushback(&particles[i+2]);
+// 		}
+// 		if (i == 1){
+// 			particles[i].neighborhood.pushback(&particles[particles.size()-1]);
+// 			particles[i].neighborhood.pushback(&particles[i-1]);
+// 			particles[i].neighborhood.pushback(&particles[i+1]);
+// 			particles[i].neighborhood.pushback(&particles[i+2]);
+// 		}
+// 		else if (i == (particles.size() - 2)){
+// 			particles[i].neighborhood.pushback(&particles[i-2]);
+// 			particles[i].neighborhood.pushback(&particles[i-1]);
+// 			particles[i].neighborhood.pushback(&particles[i+1]);
+// 			particles[i].neighborhood.pushback(&particles[0]);			
+// 		}		
+
+// 		else if (i == (particles.size() - 1)){
+// 			particles[i].neighborhood.pushback(&particles[i-2]);
+// 			particles[i].neighborhood.pushback(&particles[i-1]);
+// 			particles[i].neighborhood.pushback(&particles[0]);
+// 			particles[i].neighborhood.pushback(&particles[1]);			
+// 		}	
+
+// 		else {
+// 			particles[i].neighborhood.pushback(&particles[i-2]);
+// 			particles[i].neighborhood.pushback(&particles[i-1]);
+// 			particles[i].neighborhood.pushback(&particles[i+1]);
+// 			particles[i].neighborhood.pushback(&particles[i+2]);
+// 		}
+// 	}
+// }
 
 
 #endif
